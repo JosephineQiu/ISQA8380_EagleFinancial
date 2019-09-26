5e954337f9c0daac28123b3096334f5adcb8bb51
JosephineQiu <mqiu@unomaha.edu>
2019-09-25 23:01:53 -0500

    authviews

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
 
 # reset password urls
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_change_done.html"), name='password_reset_done'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 # change password urls
